a149ac76e7db1aa5361798f37c477dc8f3a603e8
Marc Siegel <marc@usainnov.com>
2019-11-09 17:03:51 -0500

    Parse configured primary key jmespaths from jsonl

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,51 @@
 use serde::{Serialize, Deserialize};
 use serde_json::{Deserializer, Value};
+use std::default::Default;
 use std::error::Error;
 use std::fs;
 use std::fs::File;
 use std::path::PathBuf;
-use std::default::Default;
+use std::rc::Rc;
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+struct InputConfig {
+    path: PathBuf,
+    primary_key: Option<Vec<String>>
+}
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 struct DifferConfig {
-    left: Option<PathBuf>,
-    right: Option<PathBuf>
+    left: Option<InputConfig>,
+    right: Option<InputConfig>
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Parse config from "example.yml"
-    let cfg: DifferConfig = serde_yaml::from_slice(
-        fs::read_to_string("example.yml")?.as_ref()
-    )?;
+    let cfg: DifferConfig =
+        serde_yaml::from_slice(fs::read_to_string("example.yml")?.as_ref())?;
     println!("Config read from \"example.yml\":\n    {:?}\n\n", cfg);
 
-    // Parse some jsonl values
-    let jsonl_iter = Deserializer::from_reader(
-        File::open(cfg.left.ok_or("foo")?)?
-    ).into_iter::<Value>();
+    // Convert configured primary key paths into JMESPath functions
+    let cfg_left = cfg.left.ok_or("Missing left")?;
+    let cfg_left_pks = cfg_left.primary_key.ok_or("Missing left primary key")?;
+    let jmespath_pks = cfg_left_pks.
+        iter().
+        map(|s| format!("\"{}\"", s)).                  // quoted to allow -RefId
+        map(|s| jmespath::compile(s.as_str()).unwrap()).
+        collect::<Vec<jmespath::Expression>>();
 
-    for json in jsonl_iter.take(5) {
-        println!("{}", json?.to_string())
+    // Parse primary keys from some jsonl values
+    let jsonl_iter =
+        Deserializer::from_reader(File::open(cfg_left.path)?).
+        into_iter::<Value>().
+        map(Result::unwrap);  // FIX: panics on failed parse
+
+    for json in jsonl_iter.take(5000) {
+        let pk_vals = jmespath_pks.
+            iter().
+            map(|pk| pk.search(jmespath::Variable::from(&json)).unwrap()).
+            collect::<Vec<Rc<jmespath::Variable>>>();
+        println!("{:?}", pk_vals)
     }
 
     Ok(())

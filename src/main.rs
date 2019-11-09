9e716b1674c5dc0ed59fb473539c1d8c9300c003
Marc Siegel <marc@usainnov.com>
2019-11-09 18:40:31 -0500

    Partial work to spike writing to lmdb

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rkv::{Manager, Rkv, SingleStore, StoreOptions};
 use serde::{Serialize, Deserialize};
 use serde_json::{Deserializer, Value};
 use std::default::Default;
@@ -5,6 +6,7 @@ use std::error::Error;
 use std::fs;
 use std::fs::File;
 use std::path::PathBuf;
+use tempfile::Builder;
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 struct InputConfig {
@@ -52,10 +54,44 @@ fn main() -> Result<(), Box<dyn Error>> {
     let jsonl_pks_iter =
         jsonl_iter.map(|json: Value| (extract_pk(&json), json));
 
+    // First determine the path to the environment, which is represented
+    // on disk as a directory containing two files:
+    //
+    //   * a data file containing the key/value stores
+    //   * a lock file containing metadata about current transactions
+    //
+    // In this example, we use the `tempfile` crate to create the directory.
+    //
+    let root = Builder::new().prefix("differ").tempdir().unwrap();
+    fs::create_dir_all(root.path()).unwrap();
+    let path = root.path();
+
+    // The Manager enforces that each process opens the same environment
+    // at most once by caching a handle to each environment that it opens.
+    // Use it to retrieve the handle to an opened environment—or create one
+    // if it hasn't already been opened:
+    let created_arc = Manager::singleton().write().unwrap().get_or_create(path, Rkv::new).unwrap();
+    let env = created_arc.read().unwrap();
+
+    // Then you can use the environment handle to get a handle to a datastore:
+    let store: SingleStore = env.open_single("mydb", StoreOptions::create()).unwrap();
+
+    // Use a write transaction to mutate the store via a `Writer`.
+    // There can be only one writer for a given environment, so opening
+    // a second one will block until the first completes.
+    let mut writer = env.write().unwrap();
+
+    // Keys are `AsRef<[u8]>`, while values are `Value` enum instances.
+    // Use the `Blob` variant to store arbitrary collections of bytes.
+    // Putting data returns a `Result<(), StoreError>`, where StoreError
+    // is an enum identifying the reason for a failure.
     for (pk, json) in jsonl_pks_iter {
-        println!("{}", pk);
-        println!("{}", json);
+        store.put(&mut writer, pk, &rkv::Value::Blob(json.to_string().as_bytes())).unwrap();
     }
+
+    // You must commit a write transaction before the writer goes out
+    // of scope, or the transaction will abort and the data won't persist.
+    writer.commit().unwrap();
 
     Ok(())
 }
